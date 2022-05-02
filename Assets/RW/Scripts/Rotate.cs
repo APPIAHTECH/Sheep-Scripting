07089155591709b4d6dbb2e32695fb2bd712df8b
Stephen Appiah <stephenappiahtech@gmail.com>
2022-05-02 21:12:44 +0200

    Add project first half

--- Assets/RW/Scripts/Rotate.cs
+++ Assets/RW/Scripts/Rotate.cs
@@ -1,18 +1,14 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector3 rotationSpeed;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(rotationSpeed * Time.deltaTime);
     }
 }
